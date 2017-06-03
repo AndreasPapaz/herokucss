74258ec07f4471a85d5bafcb2ea2e21d5534e972
Andreas <Andreas@Andreas.local>
2017-06-03 00:27:03 -0500

    test

--- server.js
+++ server.js
@@ -29,9 +29,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-// app.use("/static", express.static(path.join(__dirname, "public")));
-// app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static(process.cwd() + "/public"));
+// // app.use("/static", express.static(path.join(__dirname, "public")));
+// // app.use(express.static(path.join(__dirname, "/public")));
+// app.use(express.static(process.cwd() + "/public"));
+app.use(express.static("./public"));
 
 
 // set up for passport test
